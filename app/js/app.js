9836ac6ff843d6e7ecb4bfa9874b9287f22df86f
Germesych <kaiot1984@gmail.com>
2021-03-19 23:10:39 +0300

    block nav-top don

--- app/js/app.js
+++ app/js/app.js
@@ -1,20 +1,12 @@
-// // Import jQuery module (npm i jquery)
+// Import jQuery module (npm i jquery)
 // import $ from 'jquery'
 // window.jQuery = $
 // window.$ = $
 
-// // Import vendor jQuery plugin example (not module)
+// Import vendor jQuery plugin example (not module)
 // require('~/app/libs/mmenu/dist/mmenu.js')
-
-// import Swiper from 'swiper'
 import "./moduls/maps";
 import "./moduls/slider";
+import "./moduls/jq-scripts";
 document.addEventListener('DOMContentLoaded', () => {
-    // console.log('JS it work')
-    // const swiperImg = new Swiper('.swiper-container', {
-    //     loop: false,
-    //     speed: 2400, //Скорость анимации
-    //     slidesPerView: 1,
-    //     spaceBetween: 10,
-    // })
 })
