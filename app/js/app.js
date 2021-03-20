6db89942e4f71613b1e66dacf55a59e6fc6dfd92
Germesych <kaiot1984@gmail.com>
2021-03-20 04:06:41 +0300

    home page block-1 Don

--- app/js/app.js
+++ app/js/app.js
@@ -5,6 +5,7 @@
 
 // Import vendor jQuery plugin example (not module)
 // require('~/app/libs/mmenu/dist/mmenu.js')
+import "../libs/likely/likely";
 import "./moduls/maps";
 import "./moduls/slider";
 import "./moduls/jq-scripts";
